dbe45a24adaaac454bbf7b0756f7ae68416229d2
Mohammed Shakir <mohammedshakir010528@gmail.com>
2023-02-07 22:20:00 +0100

    The add/delete/edit backend functions for the comments and products are done

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const controller = require('../controllers/comments');
 const jwtauth = require("../middlewares/jwtauth");
 
-router.post("/add_comment", controller.addComment);
 router.post("/add_comment_secure/:id", jwtauth.jwtauth, controller.addCommentSecure);
+router.post("/delete_comment_secure/:id", jwtauth.jwtauth, controller.deleteCommentSecure);
+router.post("/edit_comment_secure/:id", jwtauth.jwtauth, controller.editCommentSecure);
 
 module.exports = router;
