4a484d2c0141ddd14b4ca4c56ad2d018146cabfc
SuadHus <suadreal@gmail.com>
2023-02-20 17:03:51 +0100

    Deploying fron end

--- server/routes/comments.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const controller = require('../controllers/comments');
-const jwtauth = require("../middlewares/jwtauth");
-
-router.post("/add_comment_secure/:id", jwtauth.jwtauth, controller.addCommentSecure);
-router.post("/delete_comment_secure/:id", jwtauth.jwtauth, controller.deleteCommentSecure);
-router.post("/edit_comment_secure/:id", jwtauth.jwtauth, controller.editCommentSecure);
-
-module.exports = router;
